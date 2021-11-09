7cc43ff014053d423fd39a703b5465e7af2d142f
Xiao Quan 全笑 <58939349+all-laugh@users.noreply.github.com>
2021-11-09 11:09:46 -0500

    Initial Commit

---
+++ Modes V1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Modes V1
+//
+//  Created by Xiao Quan on 11/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Modes V1/Modes_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Modes_V1App.swift
+//  Modes V1
+//
+//  Created by Xiao Quan on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Modes_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
