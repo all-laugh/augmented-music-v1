1193dd5515719d4b65fd9c623fde685a34f85a1c
Xiao Quan <58939349+all-laugh@users.noreply.github.com>
2021-11-10 15:57:30 -0500

    Add simple UI for switching modes. Filters are not implemented yet.

--- Modes V1/ContentView.swift
+++ Modes V1/ContentView.swift
@@ -9,13 +9,64 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ModesCarouselView()
     }
 }
+
+
+struct ModesCarouselView: View {
+    @State var selectedTab: Mode = PLACEHOLDER[0]
+    
+    var body: some View {
+        VStack {
+            Text("MODES")
+                .fontWeight(.bold)
+                .font(.largeTitle)
+            
+            GeometryReader { geometry in
+                let frame = geometry.frame(in: .global)
+                
+                TabView(selection: $selectedTab) {
+                    ForEach (PLACEHOLDER) { mode in
+                        VStack {
+                            Image(systemName: mode.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+//                                .padding(50)
+                                .frame(width: frame.width - 100,
+                                       height: frame.height - 100,
+                                       alignment: .center)
+                                .tag(mode)
+                                .padding(.bottom, 20)
+                            
+                            Text(mode.name)
+                                .font(.body)
+                                .fontWeight(.bold)
+                        }
+                    }
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            }
+            .frame(height: UIScreen.main.bounds.height / 2.2)
+        }
+    }
+}
+
+
+struct Mode: Identifiable, Hashable {
+    var id: Int
+    var name: String
+    var image: String
+}
+
+var PLACEHOLDER = [
+    Mode(id: 0, name: "Up In The Clouds", image: "cloud"),
+    Mode(id: 1, name: "Daily Drive", image: "figure.walk")
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+.previewInterfaceOrientation(.portrait)
     }
 }
